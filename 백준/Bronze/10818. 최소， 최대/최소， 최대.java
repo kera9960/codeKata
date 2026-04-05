ba2a109ef400d0d93287c4782ab6735287f2ec54
kera9960 <kera9960@gmail.com>
2026-04-05 15:22:09 +0900

    [Bronze III] Title: 최소, 최대, Time: 1244 ms, Memory: 109120 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,21 @@
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] array = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(array);
+        bw.write(Integer.toString(array[0]) + " " + Integer.toString(array[N-1]));
+        bw.flush();
+        bw.close();
+    }
+}
