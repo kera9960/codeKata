1d8ca014553d4ee624edd3ed9468ec1141e1659b
kera9960 <kera9960@gmail.com>
2026-05-07 10:01:35 +0900

    [level 1] Title: 소수 만들기, Time: 628.45 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;
+        for(int i =0; i<nums.length-2;i++){
+            for(int j = i+1; j<nums.length-1;j++){
+                for(int k =j+1; k<nums.length;k++){
+                    sum = nums[i]+nums[j]+nums[k];
+                    boolean isPrime = true;
+                    if(sum >= 2){
+                        for(int l =2; l<sum;l++){
+                            if(sum%l==0){
+                                isPrime = false;
+                            }
+                        }
+                        if(isPrime){
+                            answer++;
+                        }
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
